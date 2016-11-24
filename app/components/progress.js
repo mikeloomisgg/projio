ad6eec38d00f1f9d4e5ccf80d2586b786525ead6
loomismeister <Loomismeister@gmail.com>
2016-11-24 01:37:26 -0500

    Finished initial react tutorial

---
+++ app/components/progress.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class Progress extends React.Component {
+
+  render() {
+
+    return(
+      <div className="progress">
+        <span className="player__time-elapsed">{this.props.elapsed}</span>
+        <progress
+           value={this.props.position}
+           max="1"></progress>
+         <span className="player__time-total">{this.props.total}</span>
+      </div>
+    )
+  }
+
+}
+
+export default Progress
