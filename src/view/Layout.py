7ee5a96eb34bb41a71be77f8e215a0c3ed9c8941
loomismeister <Loomismeister@gmail.com>
2017-08-24 23:53:26 -0400

    Adding pyinstaller spec file and requirements.txt

--- src/view/Layout.py
+++ src/view/Layout.py
@@ -1,6 +1,7 @@
 from kivy import Config
 from kivy.graphics import Rectangle
 from kivy.graphics.context_instructions import Color
+from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 
 Config.set('graphics', 'width', 800)
@@ -15,6 +16,9 @@ class Layout(GridLayout):
             self._bg_color = Color(0, 0, 0, mode="rgba")
             self._bg_rect = Rectangle(pos=self.pos, size=self.size)
         self.bind(size=self._update_window_size)
+
+        self.button = Button(text='Hello World')
+        self.add_widget(self.button)
 
     def _update_window_size(self, root, new_size):
         self._bg_rect.size = new_size
